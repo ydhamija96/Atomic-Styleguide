69d35305c7a40f7f3eaea92919e949ef3adcc4fd
Yogesh Dhamija <yogesh@bluefountainmedia.com>
2015-06-25 12:23:53 -0400

    no message

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
 
-$json = file_get_contents('https://bitbucket.org/api/1.0/repositories/ydhamija96/nyu-bitbucket-design-nav/raw/master/');
 
-echo $json;
+$directoryListing = array();
+$url = 'https://bitbucket.org/api/1.0/repositories/ydhamija96/nyu-bitbucket-design-nav/raw/master/';
 
+
+
+function findDir($url, $currentDir){
+	$returnValue = file_get_contents($url);		// = "CONTENT/ index.php"
+	$contents = explode("\n", $returnValue);	// = ["CONTENT/", "index.php"]
+	foreach($contents as $content){
+		if($content[ count($content)-1 ] == '/'){	//Is a directory
+			echo "DIR";
+			$currentDir[$content] = findDir($url . $content, $currentDir[$content]);	// = ['CONTENT/' => [...], ...]
+		}
+		else{	//Is a file
+			$currentDir['FILE'] = $content;
+		}
+	}
+	return $currentDir;
+}
+
+findDir($url, $directoryListing);
+
+echo "<pre>";
+print_r($directoryListing);
 
 ?>
