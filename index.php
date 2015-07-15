7be352f5125c27644eeb20c2ef01aa61cf5b32c1
Yogesh Dhamija <yogesh@bluefountainmedia.com>
2015-07-15 17:06:19 -0400

    Re-pointed it to the right repo

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             $repo = $_SESSION['repo'];
         }
         else{
-            $repo = new BitBucketRepo('https://bitbucket.org/api/1.0/repositories/mricotta/nyu/raw/master/');
+            $repo = new BitBucketRepo('https://bitbucket.org/api/1.0/repositories/bluefountainmedia/NYU/raw/master/');
             $_SESSION['repo'] = $repo;
         }
 
